d377675572c93a5b85e81c2ab3797cec7503e3ca
rompeteshos <rompeteshos@hotmail.com>
2022-04-28 19:01:31 +0200

    ahora funciona

--- src/main/java/com/cursojava/curso/controllers/AuthController.java
+++ src/main/java/com/cursojava/curso/controllers/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class authController {
+public class AuthController {
 
     @Autowired
     private UsuarioDao usuarioDao;
